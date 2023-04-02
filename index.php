800c4b6eae5154de3156020c841f350e440a81f5
int <125262137+StructuredObjects@users.noreply.github.com>
2023-04-02 05:33:48 -0700

    Update index.php

--- index.php
+++ index.php
@@ -2,22 +2,22 @@
 
 class Search_Results
 {
-	public $found = false;
-	public $found_match;
+	public 		$found = false;
+	public 		$found_match; // ARR
 
-	public $closest_match = false;
-	public $closest_match_found;
+	public 		$closest_match = false;
+	public 		$closest_match_found; // ARR
 
-	public $extra_match = false;
-	public $extra_match_found;
+	public 		$extra_match = false;
+	public 		$extra_match_found; // ARR
 }
 
 class Item 
 {
-	public $name;
-	public $id;
-	public $url;
-	public $price;
+	public 		$name; // STR 
+	public 		$id; // INT
+	public 		$url; // STR
+	public 		$price; // INT
 }
 
 function set_item(array $arr): Item 
@@ -32,10 +32,11 @@ function set_item(array $arr): Item
 
 class YoworldItems
 {
-	private $data;
-	private $lines;
-	private $items;
-	private $search;
+	private 		$data;
+	private 		$lines;
+	private 		$items;
+	private 		$search;
+
 	function __construct(string $q)
 	{
 		$this->search = $q;
@@ -60,5 +61,46 @@ class YoworldItems
 			if(count($info) < 3) continue;
 			array_push($this->items, set_item($info));
 		}
+	}
+
+	public function search_item_by_name(): Search_Results
+	{
+		$s = new Search_Results();
+		foreach($this->items as $item)
+		{
+			$no_case_sen = strtolower($this->search);
+			// Case Senitive Search
+			if($item->name == $this->search) 
+			{
+				$s->found = true;
+				$s->found_match = $item;
+			} else if($no_case_sen == strtolower($item->name))
+			{
+				$s->closest_match = true;
+				$s->closest_match_found = $item;
+			}
+
+			$words = explode(" ", $no_case_sen);
+			$match = 0;
+			foreach($words as $word)
+			{
+				if(str_contains($word, strtolower($item->name)))
+					$match++;
+
+				if($match > 1) {
+					$extra_match = true;
+					array_push($extra_match_found, $item);
+				}
+			}
+		}
+
+		return $s;
+	}
+
+	public function search_by_id(): Item
+	{
+		foreach($this->items as $item)
+		{ if($item->id == $this->search) return $item; }
+		return set_item(array("", "", "", ""));
 	}
 }
